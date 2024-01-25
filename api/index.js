7ead14ea806568e271a410473375b8714874de43
vivek sharma <vvk17sharma@gmail.com>
2024-01-25 13:15:40 +0530

    CORS

--- api/index.js
+++ api/index.js
@@ -20,8 +20,7 @@ app.use(express.json()); //to parse reqest response to json
 app.use(
   cors({
     credentials: true,
-    // origin: "https://wecodeblog.vercel.app",
-    origin: "*",
+    origin: "https://wecodeblog.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     headers: ["Content-Type", "Authorization"],
   })
