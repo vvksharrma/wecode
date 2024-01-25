c529ff393a2fd1740aa6f893f3a066083787603b
vivek sharma <vvk17sharma@gmail.com>
2024-01-25 17:11:19 +0530

    CORS

--- api/index.js
+++ api/index.js
@@ -22,7 +22,18 @@ app.use(
     credentials: true,
     origin: "https://wecodeblog.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
-    headers: ["Content-Type", "Authorization"],
+    headers: [
+      "Access-Control-Allow-Headers",
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+      "token",
+      "Access-Control-Request-Method",
+      "Access-Control-Request-Headers",
+      "Access-Control-Allow-Credentials",
+    ],
   })
 );
 
